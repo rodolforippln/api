1238615533935e3c464dd39cc3733ced62d0dba6
Rodolfo Hernandez <rodolforippln@gmail.com>
2021-09-21 20:35:02 -0300

    Instalação do Strapi para API do WonGames

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c5ee853d228c19b04f8d605ac74bba54'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b2a3b2e5-5fcf-4035-814f-b65e0b2b9da4'
+};
